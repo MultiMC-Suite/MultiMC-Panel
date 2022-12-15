b79f3292093e2d8ce68a8e77198ed3d25331b511
Xen0Xys <red57101@gmail.com>
2022-12-15 11:31:33 +0100

    :rocket: Add icon component

---
+++ src/components/icon.vue
@@ -0,0 +1,38 @@
+<template>
+    <span class="icon" v-if="json" v-html="json">
+
+    </span>
+</template>
+
+<script>
+const icons = require("@/assets/mockIcon");
+export default {
+    name: "icon-component",
+    data(){
+        return{
+            json: null
+        }
+    },
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    },
+    created(){
+        if(!this.name || !icons[this.name]){
+            console.error("Icon not found");
+            return;
+        }
+        this.json = icons[this.name];
+    }
+}
+</script>
+
+<style scoped>
+    .icon{
+        height: 1.25rem;
+        aspect-ratio: 1/1;
+        color: currentColor;
+    }
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,12 +1,38 @@
 <template>
     <main>
-
+        <section class="panels">
+            <article class="panels__teams">
+                <header>
+                    <h1>Teams</h1>
+                </header>
+            </article>
+            <aside class="panels__right">
+                <article class="panels__right__notify">
+                    <header>
+                        <h1>Notifications</h1>
+                    </header>
+                </article>
+                <article class="panels__right__team">
+                    <header class="panels__right__team__header">
+                        <h1 class="panels__right__team__header__title">My Team</h1>
+                        <ul>
+                            <li>
+                                <button class="panels__right__team__header__button"><icon-component name="refresh"/></button>
+                            </li>
+                        </ul>
+                    </header>
+                </article>
+            </aside>
+        </section>
     </main>
 </template>
 
 <script>
+import IconComponent from "@/components/icon.vue";
+
 export default {
     name: "Main-View",
+    components: {IconComponent},
     beforeCreate() {
         // TODO: Check token and redirect to login if not valid
         console.log(this.$store.token)
@@ -18,6 +44,13 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    @use "../assets/colors.scss" as colors;
+    main{
+        background-color: colors.$global-background-color;
+        width: 100%;
+        min-height: 100vh;
+        display: grid;
+        place-items: center;
+    }
 </style>
