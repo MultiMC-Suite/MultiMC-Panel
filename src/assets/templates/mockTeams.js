7070f8343a52fc945f9fb13f0d8e9f8ea10b5616
Xen0Xys <red57101@gmail.com>
2022-12-19 12:20:02 +0100

    :rocket: Add dataset templates based on future api payloads

---
+++ src/assets/templates/mockTeams.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        "teamName": "Team 1",
+        "teamCode": "T1",
+        "score": 0,
+        "ownerId": 0,
+        "members": [
+            1
+        ]
+    },
+    {
+        "teamName": "Team 2",
+        "teamCode": "T2",
+        "score": 10,
+        "ownerId": 2,
+        "members": [
+            3
+        ]
+    }
+]
