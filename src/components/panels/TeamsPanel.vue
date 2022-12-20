1c9bbdc487f9475e342b10e3f6713c32a3450f73
Xen0Xys <red57101@gmail.com>
2022-12-20 11:27:10 +0100

    :rocket: Remove team sort to put it in store Translate untranslated title

--- src/components/panels/TeamsPanel.vue
+++ src/components/panels/TeamsPanel.vue
@@ -1,6 +1,6 @@
 <template>
     <article class="panel">
-        <header-component title="Teams" class="panel__header"></header-component>
+        <header-component title="Équipes" class="panel__header"></header-component>
         <div class="panel__content">
             <TeamRankingComponent v-for="(team, key) of teams" :key=key :rank=key+1 :score=team.score :team-name=team.teamName :members=team.members></TeamRankingComponent>
 <!--            <TeamRankingComponent rank="#1" :score=48 team-name="Team 1" :members="[`Xen0Xys`, `eTechVO`]"></TeamRankingComponent>-->
@@ -27,12 +27,10 @@ export default {
         this.$store.dispatch("loadTeams").then(() => {
             const teams = this.$store.teams;
             for (let team of teams) {
-                team.members = team.members.map(member => member.username);
-                team.members.push(team.ownerId.username);
-                // sort members alphabetically
+                team.members = team.members.map(member => member !== null ? member.username : "");
+                team.members.push(team.ownerId !== null ? team.ownerId.username : "");
                 team.members.sort();
             }
-            teams.sort((a, b) => b.score - a.score);
             this.teams = teams;
         });
     }
